d17478894915ccff68b4ebf5f20f7bf662856c06
Adam Wirtz <adamwirtz@Atom-Book.local>
2023-03-08 08:28:04 -0600

    add uri for production try wrong

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-part5-server-production-f42c.up.railway.app/", // change to YOUR own production server
+  uri: "wrong", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
